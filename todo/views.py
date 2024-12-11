de86a1e71b3e425745dd5a8693ebf68a4c43146b
JAHIDUL456 <1904015@student.ruet.ac.bd>
2024-12-11 21:12:07 +0600

    login

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from todo import models
+from todo.models import TODO
 
 
 def signup(request):
     if request.method=='POST':
-        fnm=request.POST.get('fmn')
+        fnm=request.POST.get('fnm')
         email=request.POST.get('email')
         psw=request.POST.get('pass')
         print(fnm, email,psw)
-    
-        
+        my_user=User.objects.create_user(fnm,email,psw)
+        my_user.save()
+        return redirect('/login')     
     return render(request,'signup.html')
+
+
+def login(request):
+    return render(request,'login.html')
