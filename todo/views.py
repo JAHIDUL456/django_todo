a4654e27e70b41e36e8bf36b13453637fbd91d50
JAHIDUL456 <1904015@student.ruet.ac.bd>
2024-12-11 16:51:04 +0600

    submit

--- todo/views.py
+++ todo/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render,redirect
 
 
 def signup(request):
+    if request.method=='POST':
+        fnm=request.POST.get('fmn')
+        email=request.POST.get('email')
+        psw=request.POST.get('pass')
+        print(fnm, email,psw)
+    
+        
     return render(request,'signup.html')
